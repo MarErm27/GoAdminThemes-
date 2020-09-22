975481db7a7eca0a3087658b64ae4cf965444718
MarErm27 <v27i72@gmail.com>
2020-09-22 14:41:28 +0300

    up

--- adminlte/template.go
+++ adminlte/template.go
@@ -403,7 +403,7 @@ var TemplateList = map[string]string{"403": `<div class="missing-content">
             <input type="text" id="{{.Field}}_end__goadmin" name="{{.Field}}_end__goadmin" value="{{.Value2}}"
                    class="form-control {{.Field}}_end__goadmin" placeholder="{{.Placeholder}}">
         </div>
-        <script> 
+        <script>
             $(function () {
                 $('input.{{.Field}}_start__goadmin').datetimepicker({{.OptionExt}});
                 $('input.{{.Field}}_end__goadmin').datetimepicker({{.OptionExt2}});
@@ -2693,7 +2693,7 @@ var TemplateList = map[string]string{"403": `<div class="missing-content">
         <div class="pull-right hidden-xs">
             <b>Theme</b> {{.System.Theme}}&nbsp;&nbsp;
         </div>
-        <strong>Powered by <a href="https://github.com/MarErm27/GoAdmin">UBCorp.PRO</a>.</strong>
+        <strong>Powered by <a href="https://github.com/MarErm27/go-admin">UBCorp.PRO</a>.</strong>
         {{.FooterInfo}}
     </footer>
 {{end}}`, "head": `{{define "head"}}
